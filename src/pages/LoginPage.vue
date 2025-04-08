8bfd3d53bcf10782a88ca36175abac71361f9e87
oog1 <oog@novicell.dk>
2025-04-08 21:35:33 +0200

    Add composables folder for future use, update LoginPage to handle POST request, and add form to template.

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,6 +1,39 @@
-<script lang="ts"></script>
+<script lang="ts">
+import { ref, reactive } from 'vue'
+
+export default {
+  setup() {
+    const { loggedIn, user, fetch: refreshSession } = useUserSession() // Inside the setup function now
+    const credentials = reactive({
+      email: '',
+      password: '',
+    })
+
+    async function login() {
+      try {
+        await $fetch('/api/login', {
+          method: 'POST',
+          body: credentials
+        })
+        await refreshSession()
+        await navigateTo('/')
+      } catch {
+        alert('Bad credentials')
+      }
+    }
+
+    return {
+      credentials,
+      login,
+    }
+  }
+}
+</script>
 
 <template>
-    <h2>Sign in</h2>
-   
+   <form @submit.prevent="login">
+    <input v-model="credentials.email" type="email" placeholder="Email">
+    <input v-model="credentials.password" type="password" placeholder="Password">
+    <button type="submit">Login</button>
+   </form>
 </template>
